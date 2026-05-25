8323a982dff636492f06c77f4f2817116d221193
Nikolas <nik.wipper@gmx.de>
2026-05-25 14:58:58 +0200

    Add match update schedule (#2)

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import load_matches
+import schedule
+import time
+
+
 def main():
-    print("Hello from ups!")
+    schedule.every().day.at("22:00").do(load_matches.main)
+    schedule.every().day.at("05:00").do(load_matches.main)
+    schedule.every().wednesday.at("21:00").do(load_matches.main)
+    schedule.every().wednesday.at("23:00").do(load_matches.main)
 
 
 if __name__ == "__main__":
-    main()
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
